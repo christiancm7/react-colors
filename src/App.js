9f5568aac32cb042edfa33ece9ae52073e576399
Christian Morales <cmorales@digiLAN.com>
2019-07-26 15:09:06 -0500

    added colorbox

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Palette {...seedColors[4]}/>
+        <Palette {...seedColors[2]}/>
       </div>
     )
   }
